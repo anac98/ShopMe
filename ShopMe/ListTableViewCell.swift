35009ab6c40a61573ae072cecb7a1a10dfef7eab
woannaju <woanna.ju@gmail.com>
2017-10-30 21:25:39 -0500

    initial commit

---
+++ ShopMe/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  ShopMe
+//
+//  Created by Joanna Wu on 10/30/17.
+//  Copyright © 2017 Joanna Wu. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
